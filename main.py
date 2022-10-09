e09baf04761eb0d579ec76330148a83b5736df42
Deveaux Benoit <contact.flapili@gmail.com>
2022-10-09 10:35:41 +0200

    setup poetry

--- main.py
+++ main.py
@@ -1,32 +1,72 @@
-import subprocess
+# coding: utf-8
+import io
 import sys
 import zipfile
-import io
+import subprocess
 from pathlib import Path
 
+import typer
 import requests
 
-pb_id = "".join(letter for letter in "".join(
-    sys.argv[1:]).lower() if letter.isalnum())
-samples_url = f"{sys.argv[1]}/file/statement/samples.zip"
-r = requests.get(samples_url)
-r.raise_for_status()
 
-with zipfile.ZipFile(io.BytesIO(r.content)) as z:
-    for file in z.filelist:
-        file_path = Path(file.filename)
-        if file_path.suffix == ".in":
-            test_input = z.read(file).decode("utf-8")
-            test_answer = z.read(f"{file_path.stem}.ans").decode("utf-8")
-            p = subprocess.run([sys.executable, sys.argv[2]],
-                               input=test_input, encoding="utf-8", capture_output=True)
-            if p.returncode != 0:
-                print("Error:", p.stderr)
-            print("Sample input:\n", test_input, sep="")
-            if p.stdout != test_answer:
-                print("Wrong answer!")
-                print("Expected:,", len(test_answer),"\n", test_answer, sep="")
-                print("Got:", len(p.stdout),"\n", p.stdout, sep="")
-            else:
-                print("Good answer!")
-                print("Answer:\n", p.stdout, sep="")
+def main(
+    script: Path = typer.Argument(
+        ...,
+        exists=True,
+        file_okay=True,
+        dir_okay=False,
+        readable=True,
+        resolve_path=True,
+        help="The script which should resolve the problem",
+    ),
+    url: str = typer.Argument(..., help="The url of the problem"),
+):
+    r = requests.get(f"{url}/file/statement/samples.zip")
+    r.raise_for_status()
+    failed = False
+
+    with zipfile.ZipFile(io.BytesIO(r.content)) as z:
+        for file in z.filelist:
+            file_path = Path(file.filename)
+            if file_path.suffix == ".in":
+                test_input = z.read(file).decode("utf-8")
+                test_answer = z.read(f"{file_path.stem}.ans").decode("utf-8")
+                p = subprocess.run(
+                    [sys.executable, str(script)],
+                    input=test_input,
+                    encoding="utf-8",
+                    capture_output=True,
+                )
+
+                if p.returncode != 0:
+                    failed = True
+                    typer.secho(f"Error on sample {file_path.stem} :\n{p.stderr}", fg="red", err=True)
+                    continue
+
+                if p.stdout != test_answer:
+                    failed = True
+                    typer.secho(f"Wrong answer for sample {file_path.stem} !", fg="red")
+
+                    if len(p.stdout) != len(test_answer):
+                        typer.secho(
+                            f"Length does not match ! Got {len(p.stdout)} characters, excepted {len(test_answer)}",
+                            fg="red",
+                        )
+                    else:
+                        char = []
+                        for a, b in zip(p.stdout, test_answer):
+                            if a == b:
+                                char.append(a)
+                            else:
+                                char.append(typer.style(a, fg="red"))
+                        typer.echo("Excepted :")
+                        typer.echo(test_answer)
+                        typer.echo("Got :")
+                        typer.echo("".join(char))
+
+    if failed is False:
+        typer.secho("Good answer !", fg="green")
+
+
+if __name__ == "__main__":
+    typer.run(main)
